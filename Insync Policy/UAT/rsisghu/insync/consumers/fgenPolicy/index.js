5428d1689c40f99c537f03f61cbbe1d92c76a7f5
Divyarani <rdivya@insillion.com>
2026-05-16 15:14:48 +0530

    GPA UAT Finalised Code

--- Insync Policy/UAT/rsisghu/insync/consumers/fgenPolicy/index.js
+++ Insync Policy/UAT/rsisghu/insync/consumers/fgenPolicy/index.js
@@ -21,11 +21,9 @@ class fgenPolicy extends twigbase {
 
     async _process_service(service, policy) {
 		
-        if (await this.__check_service_status(service, policy)){ 
-		 console.log("Return true***************** ",service.name,  policy.policy_id); return true};
+        if (await this.__check_service_status(service, policy))  return true;
 		
         await this.__add_additional_data(policy);
-		console.log("********************** After __add_additional_data", policy.policy_id)
         let ndata = await this.__transform_all(service, policy);
         if (ndata === null) return false;
         if (!(await this.__call_service(service, policy, ndata[ndata.length - 1]))) {
@@ -36,30 +34,56 @@ class fgenPolicy extends twigbase {
 
     //FAILURE
     async __get_fg_err_status(service, jx, policyId, subid, attr) {
-		let err_status = await utils.jpath_value(jx, "soapenv:Envelope.soapenv:Body?.ns2:FGUWResponseVO.status", service.target.strobjs) || '';
-		if(err_status == 'FAIL')
-			return null;
-		else
-			return "Response Error Plse check response.txt";
-		
-		
-    }
-    //SUCCESS
-    async __get_fg_status(service, jx, policyId, subid, attr) {
-        let status = await utils.jpath_value(jx, "soapenv:Envelope.soapenv:Body.ns2:FGUWResponseVO.status", service.target.strobjs);		
-		let errorMessage = '';
-		if(status && status == 'SUCCESS') errorMessage = await utils.jpath_value(jx, "soapenv:Envelope.soapenv:Body.ns2:FGUWResponseVO.successMsg", service.target.strobjs);
-		else errorMessage = await utils.jpath_value(jx, "soapenv:Envelope.soapenv:Body.ns2:FGUWResponseVO.errorDetailVOList", service.target.strobjs)|'' ;
-		console.log("******************* ",status," ** " , errorMessage);
-        let policy_no = "";	
-        if (errorMessage){
-			if(status == 'SUCCESS' || errorMessage.includes('Posted Successfully') || errorMessage.includes('Policy already Exists in Master')) {
-            policy_no = await utils.jpath_value(jx, "soapenv:Envelope.soapenv:Body.ns2:FGUWResponseVO.polNo", service.target.strobjs);
-            return policy_no;
-        } 
+		let xpath = 'soapenv:Envelope.soapenv:Body.ns2:FGUWResponseVO';
+		let val = await utils.jpath_value(jx, xpath, service.target.strobjs);
+		if (val.status === 'FAIL' || val.errorDetailVOList?.errorDesc != '') {
+			const errorDesc = val.errorDetailVOList?.errorDesc || '';
+			if (errorDesc.toLowerCase().includes('policy already exists')) {
+				return null;
+			}
+			let errors = [];
+
+			if (val.errorMsg) {
+				errors.push({
+					error_code: val.errorMsg,
+					error_desc: val.errorMsg
+				});
+			} else if (val.errorDetailVOList) {
+				// If array of errors
+				if (Array.isArray(val.errorDetailVOList)) {
+					val.errorDetailVOList.forEach(err => {
+						errors.push({
+							error_code: err.errorCode || null,
+							error_desc: err.errorDesc || null
+						});
+					});
+				} else {
+					errors.push({
+						error_code: val.errorDetailVOList.errorCode || null,
+						error_desc: val.errorDetailVOList.errorDesc || null
+					});
+				}
+			}
+
+			return val.errorMsg ? val.errorMsg : JSON.stringify(val.errorDetailVOList);
 		}
-        return null;
+		return null;
     }
+    //SUCCESS  
+    async __get_fg_status(service, jx, policyId, subid, attr) {
+
+		let xpath = 'soapenv:Envelope.soapenv:Body.ns2:FGUWResponseVO';
+		let val = await utils.jpath_value(jx, xpath, service.target.strobjs);
+		if (val.status == 'SUCCESS' || (val.status == 'FAIL' && (val.errorDetailVOList.errorDesc.toLowerCase().includes('policy already exists')))) {
+			const errorCode_exe = val?.errorMsg ? val?.errorMsg : JSON.stringify(val?.errorDetailVOList?.errorCode);
+			const errorDesc_exe = val?.errorMsg ? val?.errorMsg : JSON.stringify(val?.errorDetailVOList?.errorDesc);
+			let policy_no = await utils.jpath_value(jx, "soapenv:Envelope.soapenv:Body.ns2:FGUWResponseVO.polNo", service.target.strobjs);
+			return policy_no;
+		}
+		// console.log("failed-----");
+		return null;
+        
+    }    
 }
 
 (new fgenPolicy('fgenPolicy')).run(workerData);
